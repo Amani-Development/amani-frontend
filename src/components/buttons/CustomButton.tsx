e6ba6c1425f7d13912568a2c782e52f9bc5e2603
Paul Arokoola <parokoola@gmail.com>
2024-10-14 14:36:42 +0100

    complete search component

--- src/components/buttons/CustomButton.tsx
+++ src/components/buttons/CustomButton.tsx
@@ -1,7 +1,18 @@
-const CustomButton = () => {
-    return (
-        <></>
-    )
+interface CustomButtonProp
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  text: string;
+  icon: string;
 }
+
+const CustomButton = ({ text, icon, className }: CustomButtonProp) => {
+  return (
+    <button className={`bg-[#4C7F00] flex justify-center items-center p-4 rounded-[50px] ${className}`}>
+      <span>
+        <img src={icon} alt="" />{" "}
+      </span>
+      <span className="ml-4 text-white text-sm leading-[15px] font-bold">{text}</span>
+    </button>
+  );
+};
 
 export default CustomButton;
