01b8729bc30fe3b8403bea295a346fd224abd581
seyifunmi Odediran <philipoluseyi@gmail.com>
2024-12-23 15:33:34 +0100

    .env revert

--- src/helpers/axios.ts
+++ src/helpers/axios.ts
@@ -1,12 +1,23 @@
 import axios from "axios";
 
-const API_URL = process.env.REACT_APP_API_URL;
-const API_KEY = process.env.REACT_APP_API_KEY;
 
-console.log(API_URL, API_KEY);
-const axiosApi = axios.create({ baseURL: API_URL });
 
-axiosApi.defaults.headers.common.Authorization = `Bearer Api-Key ${API_KEY}`;
+// const API_URL = process.env.REACT_APP_API_URL;
+// const API_KEY = process.env.REACT_APP_API_KEY;
+//
+// // console.log(API_URL, API_KEY);
+// const axiosApi = axios.create({ baseURL: API_URL });
+
+//apply base url for axios
+const API_URL ='https://rankingamani.com/api';
+
+const axiosApi = axios.create({baseURL: API_URL});
+
+// let _store = store.getState().Login.user_token;
+axiosApi.defaults.headers.common.Authorization =
+  "Bearer Api-Key kt2k4wM5.7Yxzp6cz0RhhP4ggXsWFbHgZGBUwkHon";
+
+// localStorage.getItem("token") || sessionStorage.getItem("token") || "";
 
 axiosApi.interceptors.response.use(
     (response) => response,
@@ -14,21 +25,21 @@ axiosApi.interceptors.response.use(
 );
 
 export function get(url: string, config = {}) {
-    return axiosApi.get(url, { ...config });
+    return axiosApi.get(url, {...config});
 }
 
 export function post(url: string, data: object, config = {}) {
-    return axiosApi.post(url, data, { ...config });
+    return axiosApi.post(url, data, {...config});
 }
 
 export function put(url: string, data?: object, config = {}) {
-    return axiosApi.put(url, data, { ...config });
+    return axiosApi.put(url, data, {...config});
 }
 
 export function patch(url: string, data: object, config = {}) {
-    return axiosApi.patch(url, data, { ...config });
+    return axiosApi.patch(url, data, {...config});
 }
 
 export function del(url: string, config = {}) {
-    return axiosApi.delete(url, { ...config });
+    return axiosApi.delete(url, {...config});
 }
